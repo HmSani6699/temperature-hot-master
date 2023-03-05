83ce65661a1fcb81afa21770e8deb3fc3bd9ee14
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-05 11:37:21 +0600

    ad evetn handler to search button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@ const loadWeatherData = (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayWeather(data))
 }
-loadWeatherData('dhaka')
+
+const displayWeather = (data) => {
+    console.log(data)
+}
+
+
+document.getElementById('search-btn').addEventListener('click', function () {
+    const searchValue = document.getElementById('search-filed').value;
+    loadWeatherData(searchValue)
+})
+// loadWeatherData('dhaka')
