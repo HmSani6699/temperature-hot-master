7a8af49603f3e8b7464eafa1619648c0d66a2869
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-05 12:06:53 +0600

    complete to weather website

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 const API_Key = `bcc7ca3d02107c51883abd0065405bed`;
 const loadWeatherData = (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`
     fetch(url)
         .then(res => res.json())
         .then(data => displayWeather(data))
 }
 
 const displayWeather = (data) => {
-    console.log(data)
+    setInnerText('temp', data.main.temp)
+    setInnerText('clouds', data.weather[0].main)
 }
 
+//set innerText
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text
+}
 
 document.getElementById('search-btn').addEventListener('click', function () {
     const searchValue = document.getElementById('search-filed').value;
+    setInnerText('cityName', searchValue)
     loadWeatherData(searchValue)
 })
 // loadWeatherData('dhaka')
